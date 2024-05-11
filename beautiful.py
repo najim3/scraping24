797d4f5581530b667ed7d8d499ebbf8cbf99a0b4
Najim Uddin <engr.najim3@gmail.com>
2024-05-11 17:57:30 +0600

    6. Exporting data to txt file.

--- beautiful.py
+++ beautiful.py
@@ -9,5 +9,6 @@ soup = BeautifulSoup(content, 'lxml')
 box = soup.find('article', class_='main-article')
 title = box.find('h1').get_text()
 transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
-print(title)
-print(transcript)
+
+with open(f'{title}.txt', 'w') as file:
+    file.write(transcript)
