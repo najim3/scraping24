ec8257cdd3c8b03e9b53be1ec7676737ca1cdc95
Najim Uddin <engr.najim3@gmail.com>
2024-05-11 17:52:07 +0600

    5. Scrape a single page.

--- beautiful.py
+++ beautiful.py
@@ -6,4 +6,8 @@ result = requests.get(website)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
-print(soup.prettify())
+box = soup.find('article', class_='main-article')
+title = box.find('h1').get_text()
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+print(title)
+print(transcript)
