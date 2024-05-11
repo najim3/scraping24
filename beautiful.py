17b296be8188068dc7cac43524e146f89046aa98
Najim Uddin <engr.najim3@gmail.com>
2024-05-11 18:15:58 +0600

    02. Scraping multiple links within the same page.

--- beautiful.py
+++ beautiful.py
@@ -1,14 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://subslikescript.com/movie/titanic-120338'
+website = 'https://subslikescript.com/movies'
 result = requests.get(website)
 content = result.text
-
 soup = BeautifulSoup(content, 'lxml')
-box = soup.find('article', class_='main-article')
-title = box.find('h1').get_text()
-transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
 
-with open(f'{title}.txt', 'w') as file:
-    file.write(transcript)
+box = soup.find('article', class_='main-article')
+
+links = []
+for link in box.find_all('a', href=True):
+    links.append(link['href'])
+print(links)
